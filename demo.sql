134a4b9fca0e8a1757e95758e1ef7bce52a1fc80
jatinder14 <89455433+jatinder14@users.noreply.github.com>
2022-09-04 08:28:14 +0530

    added

---
+++ demo.sql
@@ -0,0 +1,69 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sep 04, 2022 at 12:45 AM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `demo`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `blog_data`
+--
+
+CREATE TABLE `blog_data` (
+  `id` int(11) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `content` longtext NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `blog_data`
+--
+
+INSERT INTO `blog_data` (`id`, `title`, `content`) VALUES
+(2, 'shyari_riya_ki', 'Riya is a shyer and a girl'),
+(7, 'Magic', 'We watch magic shows because love watching it.'),
+(9, 'hero', 'super man is my favourate hero'),
+(10, 'Techathon', 'I played my Life first hackathon at lpu under collaboration with INeuron at 3 - 4 September 2022.');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `blog_data`
+--
+ALTER TABLE `blog_data`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `blog_data`
+--
+ALTER TABLE `blog_data`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
